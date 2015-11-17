8f6fb168b62dcd72d6a08f70f569fbf917c04b57
mkain0 <kain.magali@gmail.com>
2015-11-17 14:40:59 -0300

    Add part of TestWordWrapper.

---
+++ Topic3/exercise02/WordWrapper.java
@@ -0,0 +1,20 @@
+package exercise02;
+
+import java.util.List;
+
+public class WordWrapper {
+
+	private String string;
+	private int rowLength;
+	
+	public void setStringAndRowLenght(String string, int rowLength) {
+		this.string = string;
+		this.rowLength = rowLength;		
+	}
+
+	public List<String> inputString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
