49a60ed42f2c2693cda010c19fca0f9719c980ae
mkain0 <kain.magali@gmail.com>
2015-11-18 01:11:54 -0300

    Two test working.

--- Topic3/exercise02/TestWordWrapper.java
+++ Topic3/exercise02/TestWordWrapper.java
@@ -1,6 +1,6 @@
 package exercise02;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,39 +9,32 @@ import org.junit.Test;
 
 public class TestWordWrapper {
 
-	// If the row-length is 60 and the input string is 30, the result is just the input string.
 	@Test
 	public void test60RowLengthInput30() {
 		List<String> rows = Arrays.asList("012345678901234567890123456789");
 		WordWrapper wordWrapper = new WordWrapper();
-		wordWrapper.setStringAndRowLenght("012345678901234567890123456789", 60);
-		assertEquals(rows, wordWrapper.inputString());
+		assertEquals(rows, wordWrapper.inputString("012345678901234567890123456789", 60));
 	}
-	
-    // If the row-length is 7 and the input string is "Hello Word!", the result should be: "Hello", "Word!".
+
 	@Test
 	public void test7RowLengthInputHelloWord() {
 		List<String> rows = Arrays.asList("Hello", "Word!");
 		WordWrapper wordWrapper = new WordWrapper();
-		wordWrapper.setStringAndRowLenght("Hello Word!", 60);
-		assertEquals(rows, wordWrapper.inputString());
+		assertEquals(rows, wordWrapper.inputString("Hello Word!", 7));
 	}
-	
-	// If the row-length is 3 and the input string is "a b c d e f", the result should be "a b", "c d", "e f".
+
 	@Test
 	public void test3RowLengthInputABCDEF() {
 		List<String> rows = Arrays.asList("a b", "c d", "e f");
 		WordWrapper wordWrapper = new WordWrapper();
-		wordWrapper.setStringAndRowLenght("a b c d e f", 3);
-		assertEquals(rows, wordWrapper.inputString());
+		assertEquals(rows, wordWrapper.inputString("a b c d e f", 3));
 	}
-	
-	// If the row-length is 5 and the input string is "Excelent", the result should be "Excel", "ent".
+
 	@Test
 	public void test5RowLengthInputExcelent() {
 		List<String> rows = Arrays.asList("Excel", "ent");
 		WordWrapper wordWrapper = new WordWrapper();
-		wordWrapper.setStringAndRowLenght("Excelent", 3);
-		assertEquals(rows, wordWrapper.inputString());
+		assertEquals(rows, wordWrapper.inputString("Excelent", 5));
 	}
+
 }
--- Topic3/exercise02/WordWrapper.java
+++ Topic3/exercise02/WordWrapper.java
@@ -1,20 +1,31 @@
 package exercise02;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WordWrapper {
 
-	private String string;
-	private int rowLength;
-	
-	public void setStringAndRowLenght(String string, int rowLength) {
-		this.string = string;
-		this.rowLength = rowLength;		
-	}
+	public List<String> inputString(String string, int rowLength) {
+		List<String> rows = new ArrayList<String>();
+		String[] words = string.split(" ");
 
-	public List<String> inputString() {
-		// TODO Auto-generated method stub
-		return null;
+		if (string.length() < rowLength)
+			rows.add(string);
+
+		for (String word : words) {
+			if (word.length() > rowLength) {
+				int count = (int) Math.floor((word.length() / (double) rowLength));
+				int begin = 0;
+				int end = rowLength;
+				for (int i = 0; i < count; i++) {
+					rows.add(word.substring(begin, end));
+					begin = end;
+					end += rowLength;
+				}
+				rows.add(word.substring(begin));
+			}
+		}
+		return rows;
 	}
 
 }
