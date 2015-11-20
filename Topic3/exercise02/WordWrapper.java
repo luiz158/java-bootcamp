e52d381b1df5fdc6ea6d29e85878aef38cddd2ec
magakain <kain.magali@gmail.com>
2015-11-20 11:38:55 -0300

    Pass all tests.

--- Topic3/exercise02/WordWrapper.java
+++ Topic3/exercise02/WordWrapper.java
@@ -1,30 +1,39 @@
 package exercise02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import org.apache.commons.lang.WordUtils;
 
 public class WordWrapper {
 
 	public List<String> inputString(String string, int rowLength) {
-		List<String> rows = new ArrayList<String>();
-		String[] words = string.split(" ");
+		List<String> rows = new ArrayList<String>();	
 
 		if (string.length() < rowLength)
 			rows.add(string);
 
-		for (String word : words) {
-			if (word.length() > rowLength) {
-				int count = (int) Math.floor((word.length() / (double) rowLength));
-				int begin = 0;
-				int end = rowLength;
-				for (int i = 0; i < count; i++) {
-					rows.add(word.substring(begin, end));
-					begin = end;
-					end += rowLength;
+		if(!string.contains(" ")) {
+			String[] words = string.split(" ");
+			for (String word : words) {
+				if (word.length() > rowLength) {
+					int count = (int) Math.floor((word.length() / (double) rowLength));
+					int begin = 0;
+					int end = rowLength;
+					for (int i = 0; i < count; i++) {
+						rows.add(word.substring(begin, end));
+						begin = end;
+						end += rowLength;
+					}
+					rows.add(word.substring(begin));
 				}
-				rows.add(word.substring(begin));
-			}
+			} 
+		} else {		
+			String newString = WordUtils.wrap(string, rowLength);
+			rows = Arrays.asList(newString.split("\n"));
 		}
+	
 		return rows;
 	}
 
